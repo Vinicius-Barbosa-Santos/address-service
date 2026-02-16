f7d0ff932ae0c164d7ce6aaaeae893cdeaf2b37e
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2026-02-16 20:39:17 -0300

    chore: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { env } from './env';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT);
+
+  const config = new DocumentBuilder()
+    .setTitle('Address API')
+    .setDescription('Consulta de CEP com cache')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
-bootstrap();
+
+void bootstrap();
