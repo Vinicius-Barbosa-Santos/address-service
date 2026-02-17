cf1c4b280d4d3cb4769a7f2408ab7a656fbdaf4d
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2026-02-17 09:07:50 -0300

    feat: create integration with BullMQ

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 
-@Module({})
+@Module({
+  imports: [
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'address-queue',
+    }),
+  ],
+})
 export class QueueModule {}
